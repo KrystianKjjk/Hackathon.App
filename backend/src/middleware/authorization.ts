928b61b6b57b46fc3c1d4fca897503e8674a6b67
double_s <sz.suchodolski@gmail.com>
2021-04-23 00:02:49 +0200

    add user model, controllers, routes, but still need to correct it to proper TS

---
+++ backend/src/middleware/authorization.ts
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+exports.loggedUser = function (req, res, next) {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).send({message: 'Odmowa dostępu. Operacja możliwa tylko dla zalogowanego użytkownika.'});
+
+    try {
+        const decoded = jwt.verify(token, process.env.SCHRONISKO_JWT_PRIVATE_KEY);
+        req.user = decoded;
+        next();
+    }
+    catch (ex) {
+        res.status(400).send({message: 'Nieprawidłowy token.'})
+    }
+}
+
+exports.isAdmin = function (req, res, next) {
+    if(!req.user.isAdmin) return res.status(403).send({message: 'Brak uprawnień do wykonania tej operacji.'});
+    next();
+}
