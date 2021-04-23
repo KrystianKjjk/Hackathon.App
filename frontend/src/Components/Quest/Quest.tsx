f74c50302e91342a4db782f2ebfc398b1b683110
Jan Eliasz <johnbg@tlen.pl>
2021-04-23 22:33:36 +0200

    Styles on Quest component

--- frontend/src/Components/Quest/Quest.tsx
+++ frontend/src/Components/Quest/Quest.tsx
@@ -14,15 +14,15 @@ interface QuestInterface {
 
 const testQuestObject = {
     image: "https://cdn.pixabay.com/photo/2016/11/22/19/36/arctic-wolf-1850247_1280.jpg",
-    instruction: "Czy chcesz zaprzyjaźnić się z wilkiem?",
+    instruction: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sequi alias natus blanditiis reiciendis velit ad harum repudiandae vitae esse. Czy chcesz zaprzyjaźnić się z wilkiem?",
     options: [
         {
             title: "Chcę",
-            chosenBy: ["Karol, Piotr"]
+            chosenBy: ["Karol", "Piotr"]
         },
         {
             title: "Nie chcę",
-            chosenBy: []
+            chosenBy: ["Ania"]
         },
         {
             title: "Muszę spytać mentora",
@@ -55,13 +55,21 @@ const Quest: React.FC<QuestProps> = () => {
         <div className={style.container}>
             <div className={style.imgContainer}>
                 <img src={image} alt="" />
+                <p className={style.instruction}>
+                    {instruction}
+                </p>
             </div>
-            <p className={style.instruction}>
-                {instruction}
-            </p>
+            
             <div className={style.optionsContainer}>
             {options.map((option, index) => {
-                return <p key={index}>{option.title}</p>
+                return <div key={index} className={style.singleOption}>
+                    <p className={style.optionTitle}>{option.title}</p>
+                    <ul className={style.chosenByList}>
+                        {option.chosenBy.map((item, index) => {
+                            return <li key={index}>{item}</li>
+                        })}
+                    </ul>
+                </div>
             })}
             </div>
         </div>
