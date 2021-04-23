f8bc752bfbfccafa10b29179bbf8f86cbc44b389
Klaudia Wojciechowska <k.wojc13@gmail.com>
2021-04-23 23:39:48 +0200

    login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import style from "./App.module.css";
 import Routing from "./Pages/Routing/Routing";
+import SignIn from "./Components/Login/Login";
+
 import NavigationView from './Pages/NavigationTemplate/NavigationTemplate'
 
 function App() {
-    return <Routing />;
+    return <SignIn />;
 }
 
 export default App;
