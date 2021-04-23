0d1daecf9851f39a2a49a9282975581ac7bf54ac
Jan Eliasz <johnbg@tlen.pl>
2021-04-24 00:48:29 +0200

    Ranking works

--- frontend/src/Components/Ranking/Ranking.tsx
+++ frontend/src/Components/Ranking/Ranking.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { GiBroadsword } from 'react-icons/gi'
 
 import style from './Ranking.module.css'
 
@@ -6,11 +7,58 @@ interface RankingProps {
 
 }
 
+interface userInterface {
+    name: string,
+    surname: string,
+    role: string,
+    currentGroup: string,
+    photo: string,
+    email: string,
+    password: string,
+    isAdmin: boolean,
+    totalPoints: number
+}
+
 const Ranking: React.FC<RankingProps> = () => {
-    const [users, setUsers] = useState();
+    const [users, setUsers] = useState<Array<userInterface> | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch('https://hackathon-backend-application.herokuapp.com/api/users/');
+            let data = await response.json();
+            data.users = data.users.filter((user: userInterface) => {
+                return user.isAdmin === false;
+            });
+            setUsers(data.users);
+        } catch(error) {
+            console.log(error);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    if(loading){
+        return <div>loading</div>
+    }
 
     return (
-        <div className={style.container}>123</div>
+        <div className={style.container}>
+            <h2 className={style.heading}>Ranking</h2>
+            {users!.map((user, index) => {
+                return <div key={index} className={style.rankingRow}>
+                    <span>
+                        <GiBroadsword />
+                        <span className={style.nameSpan}>{user.name}</span>
+                    </span>
+                    <span>{user.totalPoints}</span>
+                </div>
+            })}
+        </div>
     )
     };
 
