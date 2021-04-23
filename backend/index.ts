fb7bf9c8c937d4d650a734ecf5faba089d392e6a
double_s <sz.suchodolski@gmail.com>
2021-04-24 01:10:55 +0200

    add socket.io

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,9 @@
 import * as http from 'http'
 import app from './app'
+import socket from 'socket.io'
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+// const io = socket(server);
 
 server.listen(port, () => console.log(`Listening on port ${port}...`));
