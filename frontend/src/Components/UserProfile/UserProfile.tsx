3187f78f34fe52e71dfc86be42a5d0b4bdb1b7a1
Łukasz Żurawski <zurawskiu@gmail.com>
2021-04-24 01:24:10 +0200

    added img to profile

--- frontend/src/Components/UserProfile/UserProfile.tsx
+++ frontend/src/Components/UserProfile/UserProfile.tsx
@@ -3,9 +3,15 @@ import { Typography } from '@material-ui/core';
 import { Container } from './UserProfile-style';
 
 const UserProfile: React.FC = ({user}:any) => {
+
   return (
     <Container>
       <Typography variant="h2">Witaj podróżniku</Typography>
+      {user.photo ? <img src={`data:image/jpeg;base64,${Buffer.from(user.photo).toString('base64')}`} alt="avatar" style={{
+        height: '100%',
+        width: '100%',
+        objectFit: 'cover',
+    }}/> : null}
       <Typography>Imię: Piotr</Typography>
       <Typography>Nazwisko: Testowy</Typography>
       <Typography>Email: test@test</Typography>
