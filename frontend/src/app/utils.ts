7532651824830562da5f1ebd4f15393198b98781
FIlip <filip.szwedo@gmail.com>
2021-04-24 07:45:13 +0200

    fixes

--- frontend/src/app/utils.ts
+++ frontend/src/app/utils.ts
@@ -2,12 +2,14 @@ export const getUserFromLocalStorage = () => {
   const userId = localStorage.getItem("id");
   const userToken = localStorage.getItem("token");
   const userType = localStorage.getItem("type");
+  const user = localStorage.getItem("user");
 
-  return { userId, userToken, userType };
+  return { userId, userToken, userType, user };
 };
 
 export const removeUserFromLocalStorage = () => {
   localStorage.removeItem("id");
   localStorage.removeItem("token");
   localStorage.removeItem("type");
+  localStorage.removeItem("user");
 };
