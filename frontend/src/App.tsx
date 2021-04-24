2371e851558149321834d6bd2846c3b6ecf8fb55
Łukasz Żurawski <zurawskiu@gmail.com>
2021-04-24 10:44:45 +0200

    work on decisions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Routing from "./Pages/Routing/Routing";
+import UserDecisions from './Components/UserDecisionsAdminView/UserDecisionsAdminView'
 
 function App() {
-    return <Routing />;
+    return <UserDecisions />;
 }
 
 export default App;
---
+++ frontend/src/Components/UserDecisionsAdminView/UserDecisionsAdminView.tsx
@@ -0,0 +1,44 @@
+import React, {useState, useEffect } from 'react';
+
+import { Container } from './UserDecisionsAdminView-style'
+
+interface UserDecisionsAdminViewProps {
+
+}
+
+const UserDecisionsAdminView: React.FC<UserDecisionsAdminViewProps> = () => {
+    const [usersDecisions, setUsersDecisions] = useState<any>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(`https://hackathon-backend-application.herokuapp.com/api/scenarios/6083bdd60573f8c882235689`);
+            let data = await response.json();
+            console.log(data.quests)
+            setUsersDecisions(data.quests);
+        } catch(error) {
+            console.log(error);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
+console.log(usersDecisions)
+    return (
+    <Container>
+        <ul>
+            {usersDecisions.map((quest, id)=>{
+                console.log(quest.name)
+            })}
+        </ul>
+    </Container>
+    )
+    }
+
+
+    export default UserDecisionsAdminView
