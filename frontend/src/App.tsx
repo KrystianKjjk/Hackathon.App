731cf07358d63b64b22ba9e389251474dabc5845
Klaudia Wojciechowska <k.wojc13@gmail.com>
2021-04-24 08:58:49 +0200

    again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import style from "./App.module.css";
 import Routing from "./Pages/Routing/Routing";
-<<<<<<< HEAD
-import SignIn from "./Components/Login/Login";
-import UserList from './Components/UserList/UserList';
-import Loader from './Components/Loader/Loader';
-=======
 import SignIn from "./Components/Login/LoginWorkaround";
 import UserList from "./Components/UserList/UserList";
 import CreateTeamsPage from "./Pages/CreateTeamsPage/CreateTeamsPage";
->>>>>>> 28b0e64c2efe20e51fcc4437b564728f1fd81b0b
+import Loader from './Components/Loader/Loader';
 
 import NavigationView from "./Pages/NavigationTemplate/NavigationTemplate";
 
