bfd501682353106e36c41936cf0236e117e3b36f
Łukasz Żurawski <zurawskiu@gmail.com>
2021-04-24 02:53:39 +0200

    userProfile wait for props

--- frontend/src/Components/UserProfile/UserProfile.tsx
+++ frontend/src/Components/UserProfile/UserProfile.tsx
@@ -2,28 +2,29 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import { Container } from './UserProfile-style';
 
-const UserProfile: React.FC = ({user}:any) => {
-
+const UserProfile: React.FC = ({ user }: any) => {
   return (
     <Container>
       <Typography variant="h2">Witaj podróżniku</Typography>
-      {/* {user.photo ? <img src={`data:image/jpeg;base64,${Buffer.from(user.photo).toString('base64')}`} alt="avatar" style={{
-        height: '100%',
-        width: '100%',
-        objectFit: 'cover',
-    }}/> : null} */}
-      <Typography>Imię: Piotr</Typography>
-      <Typography>Nazwisko: Testowy</Typography>
-      <Typography>Email: test@test</Typography>
-      <Typography>Postać: Mag</Typography>
-      <Typography>Zespół: Potęzne donuty</Typography>
-      <Typography>Punkty: 123</Typography>
-            {/* <Typography>Imię: {user.name}</Typography>
-            <Typography>Nazwisko: {user.surname}</Typography>
-            <Typography>Email: {user.email}</Typography>
-            <Typography>Postać: {user.role}</Typography>
-            <Typography>Zespół: {user.currentGroup}</Typography>
-            <Typography>Punkty: {user.totalPoints}</Typography> */}
+      {user.photo ? (
+        <img
+          src={`data:image/jpeg;base64,${Buffer.from(user.photo).toString(
+            'base64'
+          )}`}
+          alt="avatar"
+          style={{
+            height: '100%',
+            width: '100%',
+            objectFit: 'cover',
+          }}
+        />
+      ) : null}
+      <Typography>Imię: {user.name}</Typography>
+      <Typography>Nazwisko: {user.surname}</Typography>
+      <Typography>Email: {user.email}</Typography>
+      <Typography>Postać: {user.role}</Typography>
+      <Typography>Zespół: {user.currentGroup}</Typography>
+      <Typography>Punkty: {user.totalPoints}</Typography>
     </Container>
   );
 };
