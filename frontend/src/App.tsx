1b8edef96a6b99051775db351b7038a0f859a34f
Klaudia Wojciechowska <k.wojc13@gmail.com>
2021-04-24 08:57:21 +0200

    css changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import style from "./App.module.css";
 import Routing from "./Pages/Routing/Routing";
 import SignIn from "./Components/Login/Login";
-import UserList from './Components/UserList/UserList'
+import UserList from './Components/UserList/UserList';
+import Loader from './Components/Loader/Loader';
 
 import NavigationView from './Pages/NavigationTemplate/NavigationTemplate'
 
 function App() {
-    return <Routing />;
+    return <Loader />;
 }
 
 export default App;
