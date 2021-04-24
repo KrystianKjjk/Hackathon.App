aea3bcef7227cfca8a94d7bdc3e6b37b79719838
Łukasz Żurawski <zurawskiu@gmail.com>
2021-04-24 12:10:43 +0200

    work on styles

--- frontend/src/Components/UserDecisionsAdminView/UserDecisionsAdminView-style.tsx
+++ frontend/src/Components/UserDecisionsAdminView/UserDecisionsAdminView-style.tsx
@@ -4,6 +4,15 @@ export const Container = styled.div`
 
 `;
 
+export const FlexContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    font-size: 1.25em;
+    border: 2px solid black;
+`;
+
 export const style = {
 
 };
--- frontend/src/Components/UserDecisionsAdminView/UserDecisionsAdminView.tsx
+++ frontend/src/Components/UserDecisionsAdminView/UserDecisionsAdminView.tsx
@@ -1,44 +1,91 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import { Container, FlexContainer } from './UserDecisionsAdminView-style';
+import { Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
-import { Container } from './UserDecisionsAdminView-style'
-
-interface UserDecisionsAdminViewProps {
-
-}
+interface UserDecisionsAdminViewProps {}
 
 const UserDecisionsAdminView: React.FC<UserDecisionsAdminViewProps> = () => {
-    const [usersDecisions, setUsersDecisions] = useState<any>([]);
-    const [loading, setLoading] = useState<boolean>(false);
+  const [usersDecisions, setUsersDecisions] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const fetchData = async () => {
-        setLoading(true);
-        try {
-            const response = await fetch(`https://hackathon-backend-application.herokuapp.com/api/scenarios/6083bdd60573f8c882235689`);
-            let data = await response.json();
-            console.log(data.quests)
-            setUsersDecisions(data.quests);
-        } catch(error) {
-            console.log(error);
-        }
-        setLoading(false);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://hackathon-backend-application.herokuapp.com/api/scenarios/6083bdd60573f8c882235689`
+      );
+      let data = await response.json();
+      console.log(data.quests);
+      setUsersDecisions(data.quests);
+    } catch (error) {
+      console.log(error);
     }
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  console.log('calość', usersDecisions);
+  return (
+    <>
+      <Grid container direction="row">
+        {usersDecisions.map((quest: any, id: number) => {
+          return (
+            <Grid container item justify="center" xs={12} alignContent='center'>
+              <Typography variant="h2" key={id}>
+                <span>Zadanie:</span> {quest.name}
+              </Typography>
+              <Grid container item justify="center" xs={12}>
+                {quest.decisions.map((decisions: any, id: number) => {
+                  return (
+                    <FlexContainer key={id}>
+                      <h3>
+                        <span>Ścieżka: </span> {decisions.title}
+                      </h3>
+                      <ListItem>
+                        <span>Ryzyko decyzji: </span>
+                        {decisions.risk}
+                      </ListItem>
+                      <ListItem>
+                        <span>Kara za podjęcie decyzji:</span>
+                        {decisions.punishment}
+                      </ListItem>
+                      <ListItem>
+                        <span>Nagroda:</span> {decisions.prize}
+                      </ListItem>
 
-console.log(usersDecisions)
-    return (
-    <Container>
-        <ul>
-            {usersDecisions.map((quest: any, id: number)=>{
-                console.log(quest.name)
-            })}
-        </ul>
-    </Container>
-    )
-    }
+                      <Grid container item justify="center" xs={12}>
+                        {decisions.users.map((user: any, id: number) => {
+                          return (
+                            <FlexContainer key={id}>
+                              <h4 key={id}>Użytkownicy</h4>
+                              <ListItem>
+                                <span>Imie:</span> {user.name}
+                              </ListItem>
+                              <ListItem>
+                                <span>Nazwisko: </span> {user.surname}
+                              </ListItem>
+                              <ListItem>
+                                <span>Id użytkownika: </span> {user._id}
+                              </ListItem>
+                            </FlexContainer>
+                          );
+                        })}
+                      </Grid>
+                    </FlexContainer>
+                  );
+                })}
+              </Grid>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
 
-
-    export default UserDecisionsAdminView
+export default UserDecisionsAdminView;
